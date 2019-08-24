3ef006e4b46ae4cd41dd2affa114a0086e4cfc71
Adam <adamjseidman@gmail.com>
2019-08-24 18:34:54 -0400

    added some sponsor stuff

--- assets/js/sponsors.js
+++ assets/js/sponsors.js
@@ -1,12 +1,17 @@
 var sponsorlist = {
-    "platinum":["Kaiser.png", "Master_Cam.png", "PCC.png", "Solidworks.png", "Monster.png", "RIT.png", "Student_Gov.png", "Magna.png", "National_Instruments.png", "Rochester_Gear.png"],
-    "gold":["Admar.jpg", "Blue_Origin.jpg", "Duke.jpg", "Harris.jpg", "Kell_tech.jpg", "Mahany.jpg", "Millennium.jpg", "TW_Metals.jpg"],
-    "silver":["A&K.jpg", "Federal_Mogul_Motorparts.jpg", "Kenwell.jpg", "Polaris.jpg", "Smidgens.jpg", "Advanced_Auto.jpg", "GE_Aviation.jpg", "McGard.jpg", "RSTW.jpg", "TMF.jpg", "Botto_Brothers.jpg", "Gleason.jpg", "NTN.jpg", "Romold.jpg", "igus.jpg", "Cummins.jpg", "KOYO.jpg", "PCB.jpg", "SGS.jpg"],
+    "platinum":["Altair.png", "Ansys.png", "Harveytool.png", "Helical.png", "Mahany.png", "Parker.png", "Toyota.png", "Kaiser.png", "Master_Cam.png", "PCC.png", "Solidworks.png", "Monster.png", "RIT.png", "Student_Gov.png", "Magna.png", "National_Instruments.png", "Rochester_Gear.png"],
+    "gold":["A&K.jpg", "Admar.jpg", "Blue_Origin.jpg", "Duke.jpg", "Harris.jpg", "Kell_tech.jpg", "Millennium.jpg", "TW_Metals.jpg"],
+    "silver":["Federal_Mogul_Motorparts.jpg", "Kenwell.jpg", "Polaris.jpg", "Smidgens.jpg", "Advanced_Auto.jpg", "GE_Aviation.jpg", "McGard.jpg", "RSTW.jpg", "TMF.jpg", "Botto_Brothers.jpg", "Gleason.jpg", "NTN.jpg", "Romold.jpg", "igus.jpg", "Cummins.jpg", "KOYO.jpg", "PCB.jpg", "SGS.jpg"],
     "bronze":["Autoparts.jpg", "Car_parts.jpg", "Dibellas.jpg", "JC_Whitney.jpg", "SKF.jpg", "US_Autoparts.jpg", "Bolt.jpg", "Diamond_wire.jpg", "Harvey_Engineering.jpg", "MITEE-BITE.jpg", "TLF.jpg"]
 };
 
 
 function createSponsorLink() {
+    sponsorlist.platinum = sponsorlist.platinum.sort();
+    sponsorlist.gold = sponsorlist.gold.sort();
+    sponsorlist.silver = sponsorlist.silver.sort();
+    sponsorlist.bronze = sponsorlist.bronze.sort();
+    console.log(sponsorlist);
     function sponsorimg(tier, link) {
         name = link.split('.')[0];
         if (name.includes('_')) name = name.split('_').join(" ");
