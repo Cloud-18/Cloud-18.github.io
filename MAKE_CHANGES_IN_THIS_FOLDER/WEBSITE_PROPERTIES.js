2ee3649c15ca9f11e58444c895bd4cda7592aa1d
Adam Seidman <adamjseidman@gmail.com>
2019-10-21 09:29:25 -0400

    Remove Baja Site

--- MAKE_CHANGES_IN_THIS_FOLDER/WEBSITE_PROPERTIES.js
+++
@@ -1,8 +0,0 @@
-const year = 20; // AS IN R20
-const eboard_positions = [
-  "Team Manager",
-  "Project Manager",
-  "Treasurer",
-  "Secretary"
-]; // Only touch these if they are actually changed
-var newsBuffer = 72;
